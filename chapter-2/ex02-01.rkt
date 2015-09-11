57154d6046dd3cc1340cc0bc61696b0482a9c5b5
m00nlight <dot_wangyushi@yeah.net>
2015-09-11 20:39:52 +0800

    Finish factorial in bignum in exercise 02-01

--- chapter-2/ex02-01.rkt
+++ chapter-2/ex02-01.rkt
@@ -62,6 +62,71 @@
       [else (cons (- (car bignum) 1) (cdr bignum))])))
 
 
+(check-expect (num->bigint 258) '(2 0 1))
+(check-expect (num->bigint 16) '(0 1))
 
+;; num->bigint : Int -> Bigint
+;; descp : Normal number to bigint representation
+;; usage : (num->bigint 258) = '(2 0 1)
+(define num->bigint
+  (lambda (num)
+    (if (zero? num)
+        (zero)
+        (cons (modulo num N) (num->bigint (quotient num N))))))
+
+(check-expect (bigint->num '(2 0 1)) 258)
+(check-expect (bigint->num '(0 1)) 16)
+(check-expect (bigint->num '()) 0)
+;; bigint->num : Bigint -> Int
+;; descp : Bigint representation to normal number in racket
+;; usage : (bigint->num '(2 0 1)) = 258
+;; usage : (bigint->num '(0 1)) = 16
+
+(define bigint->num
+  (lambda (bigint)
+    (cond
+      [(is-zero? bigint) 0]
+      [else (+ (car bigint) (* N (bigint->num (cdr bigint))))])))
+
+
+(check-expect (add '(0 1) '(0 1)) '(0 2))
+(check-expect (add '(1) '(15)) '(0 1))
+;; add : Bignum * Bignum -> Bignum
+;; descp : Add two number in bignum represenation
+(define add
+  (lambda (num1 num2)
+    (if (is-zero? num1)
+        num2
+        (successor (add (predecessor num1) num2)))))
+
+
+(check-expect (multiply (num->bigint 16) (num->bigint 16))
+              (num->bigint 256))
+(check-expect (multiply (num->bigint 2) (num->bigint 64))
+              (num->bigint 128))
+
+;; multiply : Bigint * Bigint -> Bigint
+;; descp : Multiply two bigint numbers
+(define multiply
+  (lambda (num1 num2)
+    (if (is-zero? num1)
+        (zero)
+        (add num2 (multiply (predecessor num1) num2)))))
+
+
+(check-expect (factorial (num->bigint 3))
+              (num->bigint 6))
+(check-expect (factorial (num->bigint 4))
+              (num->bigint 24))
+(check-expect (factorial (num->bigint 10))
+              (num->bigint 3628800))
+
+;; factorial : Bigint -> Bigint
+;; descp : Calculate the bigint representation of n!.
+(define factorial
+  (lambda (num)
+    (if (is-zero? num)
+        (successor num)
+        (multiply num (factorial (predecessor num))))))
 
 (test)
